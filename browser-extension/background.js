bcffd0636747e67eb9066d0d2af68a9b8627af11
Totalsimplicity <leonardo.kulon@gmail.com>
2025-07-09 20:44:17 -0400

    scrape tab in the bg & open it if not opened.

--- browser-extension/background.js
+++ browser-extension/background.js
@@ -2,23 +2,33 @@ browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.type === "GET_ASSIGNMENT") {
     const assignmentUrl = `https://wilton.schoology.com/assignment/${request.assignmentId}/info`;
 
-    // Find the tab with the assignment open
     browser.tabs.query({ url: assignmentUrl }, (tabs) => {
       if (tabs.length > 0) {
-        // Send a message to the content script in that tab to scrape the data
         browser.tabs.sendMessage(
           tabs[0].id,
           { type: "SCRAPE_ASSIGNMENT" },
-          (response) => {
-            sendResponse({ data: response.data });
-          }
+          (response) => sendResponse({ data: response.data })
         );
       } else {
-        sendResponse({ data: "Assignment page not open in any tab." });
+        browser.tabs.create({ url: assignmentUrl, active: false }, (tab) => {
+          function handleUpdated(tabId, changeInfo) {
+            if (tabId === tab.id && changeInfo.status === "complete") {
+              browser.tabs.onUpdated.removeListener(handleUpdated);
+              browser.tabs.sendMessage(
+                tab.id,
+                { type: "SCRAPE_ASSIGNMENT" },
+                (response) => {
+                  sendResponse({ data: response.data });
+                  browser.tabs.remove(tab.id);
+                }
+              );
+            }
+          }
+          browser.tabs.onUpdated.addListener(handleUpdated);
+        });
       }
     });
 
-    // Indicate async response
     return true;
   }
 });
